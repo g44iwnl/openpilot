e067981576e4001e7a3374ab1679230d66789c5a
ajouatom <ajouatom@gmail.com>
2025-04-03 22:18:49 +0900

    test

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -418,7 +418,7 @@ class CarState(CarStateBase):
       if "HDA_INFO_4A3" in cp.vl:
         self.hda_info_4a3 = copy.copy(cp.vl.get("HDA_INFO_4A3", {}))
         ret.speedLimit = self.hda_info_4a3["SPEED_LIMIT"]
-        print("speedLimit = {}".format(ret.speedLimit))
+        print(self.hda_info_4a3, ret.speedLimit)
         
       if "NEW_MSG_4B4" in cp.vl:
         self.new_msg_4b4 = copy.copy(cp.vl.get("NEW_MSG_4B4", {}))
